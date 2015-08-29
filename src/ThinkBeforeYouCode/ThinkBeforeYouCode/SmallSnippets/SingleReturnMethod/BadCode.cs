a835cdffa98ed24683bf3f48e7f7531bac8aa881
Rushabh Dadbhawala <rdadbhawala@outlook.com>
2015-08-29 15:26:20 +0530

    Single Return Method

---
+++ src/ThinkBeforeYouCode/ThinkBeforeYouCode/SmallSnippets/SingleReturnMethod/BadCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.ThinkBeforeYouCode.SmallSnippets.SingleReturnMethod
+{
+	public class BadCode
+	{
+		public BadCode()
+		{ }
+
+		public double ComputeTax(double income)
+		{
+			if (income < 10000)
+			{
+				return income * 10f / 100f;
+			}
+			else if (income < 20000)
+			{
+				return income * 20f / 100f;
+			}
+			else
+			{
+				return income * 30f / 100f;
+			}
+		}
+	}
+}
---
+++ src/ThinkBeforeYouCode/ThinkBeforeYouCode/SmallSnippets/SingleReturnMethod/GoodCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.ThinkBeforeYouCode.SmallSnippets.SingleReturnMethod
+{
+	public class GoodCode
+	{
+		public double ComputeTax(double income)
+		{
+			double tax = 0;
+			if (income < 10000)
+			{
+				tax = income * 10f / 100f;
+			}
+			else if (income < 20000)
+			{
+				tax = income * 20f / 100f;
+			}
+			else
+			{
+				tax = income * 30f / 100f;
+			}
+			return tax;
+		}
+	}
+}
