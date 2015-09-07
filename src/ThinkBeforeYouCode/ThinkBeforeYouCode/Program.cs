33820d61d610163c55cf632db27512ac605071a7
Rushabh Dadbhawala <rdadbhawala@outlook.com>
2015-09-07 21:37:07 +0530

    commented code and wrote new algorithm

--- src/ThinkBeforeYouCode/ThinkBeforeYouCode/Program.cs
+++ src/ThinkBeforeYouCode/ThinkBeforeYouCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Abstraction.ThinkBeforeYouCode.MultiLineComments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace ThinkBeforeYouCode
 	{
 		static void Main(string[] args)
 		{
+			int[] arr = new int[] { 6, 4, 5, 4, 3, 7, 2 };
+			SortingAlgorithm<int> algo = new SortingAlgorithm<int>();
+			int[] sortedArray = algo.Sort(arr);
+			foreach (int item in sortedArray)
+			{
+				Console.Write(item + " ");
+			}
+			Console.ReadLine();
 		}
 	}
 }
