1cd10a993ca2b8777cb3f2260bf66dd4c514a822
Rushabh Dadbhawala <rdadbhawala@outlook.com>
2015-09-07 21:38:39 +0530

    commented code and new algo

--- src/ThinkBeforeYouCode/ThinkBeforeYouCode/MultiLineComments/BubbleSort.cs
+++ src/ThinkBeforeYouCode/ThinkBeforeYouCode/MultiLineComments/BubbleSort.cs
@@ -11,6 +11,7 @@ namespace Abstraction.ThinkBeforeYouCode.MultiLineComments
 	{
 		public T[] Sort(T[] unsortedArray)
 		{
+			/*
 			int arrLength = unsortedArray.Length;
 			int outerCtr = 0;
 			int innerCtr = 0;
@@ -35,7 +36,34 @@ namespace Abstraction.ThinkBeforeYouCode.MultiLineComments
 			}
 
 			return sortedArray;
+			 * */
 
+			int arrLength = unsortedArray.Length;
+			int sortedItemsCount = 0;
+			int insertIndex = 0;
+			int diff = 0;
+
+			T[] sortedArray = new T[arrLength];
+			for (sortedItemsCount = 0; sortedItemsCount < arrLength; sortedItemsCount++)
+			{
+				T unsortedItem = unsortedArray[sortedItemsCount];
+				for (insertIndex = sortedItemsCount; insertIndex > 0; insertIndex--)
+				{
+					T sortedItem = sortedArray[insertIndex - 1];
+					diff = sortedItem.CompareTo(unsortedItem);
+					if (diff > 0)
+					{
+						sortedArray[insertIndex] = sortedItem;
+					}
+					else
+					{
+						break;
+					}
+				}
+				sortedArray[insertIndex] = unsortedItem;
+			}
+
+			return sortedArray;
 		}
 	}
 }
