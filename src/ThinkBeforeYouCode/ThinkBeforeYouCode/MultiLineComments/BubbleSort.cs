28ab1671c5d22b45fbe6fc5439c44f6399d4ba5e
Rushabh Dadbhawala <rdadbhawala@outlook.com>
2015-09-13 08:13:09 +0530

    single line comment

--- src/ThinkBeforeYouCode/ThinkBeforeYouCode/MultiLineComments/BubbleSort.cs
+++ src/ThinkBeforeYouCode/ThinkBeforeYouCode/MultiLineComments/BubbleSort.cs
@@ -11,30 +11,32 @@ namespace Abstraction.ThinkBeforeYouCode.MultiLineComments
 	{
 		public T[] Sort(T[] unsortedArray)
 		{
-			int arrLength = unsortedArray.Length;
-			int outerCtr = 0;
-			int innerCtr = 0;
+			//int arrLength = unsortedArray.Length;
+			//int outerCtr = 0;
+			//int innerCtr = 0;
 
-			T[] sortedArray = new T[arrLength];
-			Array.Copy(unsortedArray, sortedArray, arrLength);
+			//T[] sortedArray = new T[arrLength];
+			//Array.Copy(unsortedArray, sortedArray, arrLength);
 
-			for (outerCtr = 0; outerCtr < arrLength; outerCtr++)
-			{
-				for (innerCtr = outerCtr + 1; innerCtr < arrLength; innerCtr++)
-				{
-					T outerItem = sortedArray[outerCtr];
-					T innerItem = sortedArray[innerCtr];
+			//for (outerCtr = 0; outerCtr < arrLength; outerCtr++)
+			//{
+			//	for (innerCtr = outerCtr + 1; innerCtr < arrLength; innerCtr++)
+			//	{
+			//		T outerItem = sortedArray[outerCtr];
+			//		T innerItem = sortedArray[innerCtr];
 
-					int diff = outerItem.CompareTo(innerItem);
-					if (diff > 0)
-					{
-						sortedArray[innerCtr] = outerItem;
-						sortedArray[outerCtr] = innerItem;
-					}
-				}
-			}
+			//		int diff = outerItem.CompareTo(innerItem);
+			//		if (diff > 0)
+			//		{
+			//			sortedArray[innerCtr] = outerItem;
+			//			sortedArray[outerCtr] = innerItem;
+			//		}
+			//	}
+			//}
 
-			return sortedArray;
+			//return sortedArray;
+
+			throw new NotImplementedException();
 
 		}
 	}
